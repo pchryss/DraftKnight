3f123fe9ca3ac9ca7de403d2cc062b4eb9b38e4c
pchryss <philipchryssochoos@gmail.com>
2025-07-11 23:57:58 -0400

    Initial Commit

---
+++ DraftKnight/DraftKnightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraftKnightApp.swift
+//  DraftKnight
+//
+//  Created by Philip Chryssochoos on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DraftKnightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DraftKnightTests/DraftKnightTests.swift
@@ -0,0 +1,17 @@
+//
+//  DraftKnightTests.swift
+//  DraftKnightTests
+//
+//  Created by Philip Chryssochoos on 7/11/25.
+//
+
+import Testing
+@testable import DraftKnight
+
+struct DraftKnightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
